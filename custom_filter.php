d5d1a52b182bdd86b12d9f724715643674f944a4
Rana Umair <rjee4631@gmail.com>
2022-05-06 17:59:44 +0500

    Update custom_filter.php

--- custom_filter.php
+++ custom_filter.php
@@ -9,6 +9,194 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
+<style>
+    .page-breadcrumbs {
+        display: none !important;
+    }
+
+    .row.cls-spacer {
+        padding-top: 60px;
+        padding-bottom: 60px;
+    }
+
+    .spl-demo-page .post h3 {
+        line-height: normal;
+        font-size: 1rem;
+        margin: .75rem .33rem;
+        letter-spacing: normal;
+        margin-bottom: 0;
+        min-height: 70px;
+        display: block;
+    }
+
+    .row.cls-spacer.end-secline {
+        border-top: 1px solid #e9e9e9;
+    }
+
+    .cls-spacer.end-secline .post h3 {
+        font-size: 14px;
+        min-height: 60px;
+    }
+
+    .cls-spacer.end-secline .post.box {
+        min-height: 240px;
+        margin-bottom: 30px;
+    }
+
+    .bg-page-nws h2 {
+        color: #fff;
+        font-family: Open Sans Condensed, sans-serif;
+        font-size: 40px;
+        font-weight: 700;
+        line-height: 1.3;
+        margin-bottom: 12px;
+        position: relative;
+        text-shadow: 2px 2px 2px rgba(0, 0, 0, .35);
+        text-transform: uppercase;
+    }
+
+    .bg-page-nws .txt-orange {
+        font-size: 22px;
+        line-height: normal;
+        text-transform: uppercase;
+        text-shadow: 1px 1px 1px rgba(0, 0, 0, .15);
+    }
+
+    .bg-page-nws .button {
+        font-family: Open Sans Condensed, sans-serif;
+    }
+
+    .spl-main-page {
+        background-color: #014690;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+    }
+
+    .bg-page-nws {
+        display: flex;
+        width: 100%;
+        max-width: 1200px;
+        margin-right: auto;
+        margin-left: auto;
+        min-height: 480px;
+        align-items: center;
+    }
+
+    .bg-page-nws .bg-page-nws-inner {
+        display: block;
+        max-width: 640px;
+        width: 100%;
+        padding: 0 15px;
+    }
+
+    .txt-orange {
+        color: #f50;
+    }
+
+    .filter_rc_nws h4 {
+        display: block;
+        color: #505050;
+        margin-top: 25px;
+        font-size: 18px;
+        line-height: normal;
+        margin-bottom: 15px;
+    }
+
+    .filter_rc_nws ul {
+        display: block;
+        margin: 0;
+        padding: 0;
+    }
+
+    .filter_rc_nws {
+        display: block;
+        padding-left: 12px;
+    }
+
+    .filter_rc_nws ul li {
+        display: block;
+        list-style: none;
+        margin-left: 2px;
+        line-height: normal;
+        font-size: 15px;
+        position: relative;
+        margin-bottom: 8px;
+        cursor: pointer;
+    }
+
+    .filter_rc_nws ul li input {
+        margin-right: 8px;
+    }
+
+    .filter_rc_nws ul li input {
+        margin-right: 0;
+        margin-bottom: 0;
+        position: absolute;
+        left: 0;
+        right: 0;
+        display: block;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        opacity: 0;
+        cursor: pointer;
+        height: 100%;
+    }
+
+    .filter_rc_nws ul li::before {
+        content: "";
+        display: inline-block;
+        width: 15px;
+        height: 15px;
+        background: transparent;
+        margin-right: 8px;
+        line-height: normal;
+        border-radius: 2px;
+        border: 2px solid #505050;
+        position: relative;
+        top: 2px;
+    }
+
+    .filter_rc_nws ul li.activefilter::after {
+        content: "";
+        display: block;
+        background: #ff5500;
+        width: 8px;
+        height: 8px;
+        position: absolute;
+        top: 7px;
+        left: 3.5px;
+    }
+
+    @media screen and (max-width:900px) {
+        .row.cls-spacer.end-secline {
+            display: flex;
+            flex-wrap: wrap;
+            flex-direction: column-reverse;
+        }
+
+        .row.cls-spacer .small-9.columns {
+            width: 100%;
+        }
+
+        .row.cls-spacer .small-3.columns {
+            width: 100%;
+        }
+
+        .filter_rc_nws {
+            display: block;
+            padding-left: 15px;
+            margin-bottom: 40px;
+        }
+    }
+
+    @media screen and (max-width:480px) {
+        .bg-page-nws h2 {
+            font-size: 25px;
+        }
+    }
+</style>
 
 <body>
 
@@ -296,48 +484,46 @@
     </section>
 
     <script>
-    jQuery(document).ready(function(){
-        jQuery(".filter_rc_nws ul li").click(function(){
-            jQuery(this).toggleClass("activefilter");
-        });
-        
-        //  NWS- filter checked box function work ----
-        
-        
-        var checked_boxes = jQuery(".filter_rc_nws ul li").length;
-        setInterval(()=>{
-            if( jQuery('.filter_rc_nws ul li input:checked').length  == 0 ){
-                jQuery(".post-archive-content .filter-items-nws").css('display', 'block');
-            }
-        },1000);
-        for (let index = 0; index < checked_boxes; index++) {
-            var check_uniq_id = null;
-            var check_uniq_id = jQuery(jQuery(".filter_rc_nws ul li")[index]).attr('data-cat');
-            
-            jQuery(".filter_rc_nws ul li[data-cat='"+ check_uniq_id +"'] input").on("click",function() {
-                
-                if (jQuery(".filter_rc_nws ul li[data-cat='"+ check_uniq_id +"'] input").is(":checked")) {
-                    jQuery(".post-archive-content .filter-items-nws[data-cat-"+ check_uniq_id +"='true']").css('display', 'block');
-                } else if (jQuery(".filter_rc_nws ul li[data-cat='"+ check_uniq_id +"'] input").is(":not(:checked)")) {
-                    jQuery(".post-archive-content .filter-items-nws[data-cat-"+ check_uniq_id +"='true']").css('display', 'none');
-                }
-                for (let index = 0; index < checked_boxes; index++) {
-                    var check_uniq_id = null;
-                    var check_uniq_id = jQuery(jQuery(".filter_rc_nws ul li")[index]).attr('data-cat');
-                    if (jQuery(".filter_rc_nws ul li[data-cat='"+ check_uniq_id +"'] input").is(":checked")) {
-                        jQuery(".post-archive-content .filter-items-nws[data-cat-"+ check_uniq_id +"='true']").css('display', 'block');
-                    } else if (jQuery(".filter_rc_nws ul li[data-cat='"+ check_uniq_id +"'] input").is(":not(:checked)")) {
-                        jQuery(".post-archive-content .filter-items-nws[data-cat-"+ check_uniq_id +"='true']").css('display', 'none');
-                    }
-                }
+        jQuery(document).ready(function() {
+            jQuery(".filter_rc_nws ul li").click(function() {
+                jQuery(this).toggleClass("activefilter");
             });
-        }
-        
-        
-    });
-    
-    
-</script>
+
+            //  NWS- filter checked box function work ----
+
+
+            var checked_boxes = jQuery(".filter_rc_nws ul li").length;
+            setInterval(() => {
+                if (jQuery('.filter_rc_nws ul li input:checked').length == 0) {
+                    jQuery(".post-archive-content .filter-items-nws").css('display', 'block');
+                }
+            }, 1000);
+            for (let index = 0; index < checked_boxes; index++) {
+                var check_uniq_id = null;
+                var check_uniq_id = jQuery(jQuery(".filter_rc_nws ul li")[index]).attr('data-cat');
+
+                jQuery(".filter_rc_nws ul li[data-cat='" + check_uniq_id + "'] input").on("click", function() {
+
+                    if (jQuery(".filter_rc_nws ul li[data-cat='" + check_uniq_id + "'] input").is(":checked")) {
+                        jQuery(".post-archive-content .filter-items-nws[data-cat-" + check_uniq_id + "='true']").css('display', 'block');
+                    } else if (jQuery(".filter_rc_nws ul li[data-cat='" + check_uniq_id + "'] input").is(":not(:checked)")) {
+                        jQuery(".post-archive-content .filter-items-nws[data-cat-" + check_uniq_id + "='true']").css('display', 'none');
+                    }
+                    for (let index = 0; index < checked_boxes; index++) {
+                        var check_uniq_id = null;
+                        var check_uniq_id = jQuery(jQuery(".filter_rc_nws ul li")[index]).attr('data-cat');
+                        if (jQuery(".filter_rc_nws ul li[data-cat='" + check_uniq_id + "'] input").is(":checked")) {
+                            jQuery(".post-archive-content .filter-items-nws[data-cat-" + check_uniq_id + "='true']").css('display', 'block');
+                        } else if (jQuery(".filter_rc_nws ul li[data-cat='" + check_uniq_id + "'] input").is(":not(:checked)")) {
+                            jQuery(".post-archive-content .filter-items-nws[data-cat-" + check_uniq_id + "='true']").css('display', 'none');
+                        }
+                    }
+                });
+            }
+
+
+        });
+    </script>
 
 </body>
 
